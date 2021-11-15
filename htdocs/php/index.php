5e069279fba1aac4fec96439e849e51dddc37e05
liuxiaowei01 <liuxiaowei01@58.com>
2021-11-15 19:12:41 +0800

    配置master主从

--- htdocs/php/index.php
+++ htdocs/php/index.php
@@ -1,3 +1,27 @@
 <?php
 
+
+
+
+$a = 2.30;
+echo json_encode($a);die;
+
+
+
+var_dump(strtotime("-1 year"));
+
+die;
+
+
+$a = -1;
+
+if($a==0){
+	die('ok');
+}else{
+	die('no');
+}
+
+
+
+
 phpinfo();
